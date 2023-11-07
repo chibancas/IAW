0774c085e2a36d96c0d00383e4e568c3c3dac81c
chibancas <juanjosefdz2003@gmail.com>
2023-11-07 20:56:06 +0000

    no funca l get

--- api-libreria/src/app.module.ts
+++ api-libreria/src/app.module.ts
@@ -2,22 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeedModule } from './modulos/seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { AutoresModule } from './modulos/autores/autores.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      // type: 'postgres',
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // database: process.env.DB_NAME,
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      // autoLoadEntities: true,
+      // synchronize: true
       type:'postgres',
-      host:process.env.DB_HOST,
-      port:+process.env.DB_PORT,
-      database:process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      host:"192.168.1.213",
+      port:5433, //poniendo un + convertimos cualquier variable a numero
+      database:"apiLibreria",
+      username: "postgres",
+      password: "usuario",
       autoLoadEntities:true,
       synchronize:true
-
-
-
-    })
+    }),
+    SeedModule,
+    AutoresModule
   ],
   controllers: [AppController],
   providers: [AppService],
